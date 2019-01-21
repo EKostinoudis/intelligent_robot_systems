4e5017b21d41aa2352b9f90eb53baffdf221a203
EKostinoudis <vagelis789@gmail.com>
2019-01-22 00:47:47 +0200

    Complete Challenge 6

--- art_autonomous_exploration/src/target_selection.py
+++ art_autonomous_exploration/src/target_selection.py
@@ -25,6 +25,7 @@ class TargetSelection:
         self.omega = 0.0
         self.radius = 0
         self.method = selection_method
+        self.previous_target = [-1, -1]
 
         self.brush = Brushfires()
         self.topo = Topology()
@@ -64,7 +65,7 @@ class TargetSelection:
         nodes = self.topo.topologicalNodes(ogm, skeleton, coverage, origin, \
                 resolution, brush, ogm_limits)
         Print.art_print("Topo nodes time: " + str(time.time() - tinit), Print.ORANGE)
-        
+
         # Visualization of topological nodes
         vis_nodes = []
         for n in nodes:
@@ -85,9 +86,103 @@ class TargetSelection:
         # Random point
         if self.method == 'random' or force_random == True:
           target = self.selectRandomTarget(ogm, coverage, brush, ogm_limits)
-        ########################################################################
+        
+        # Smart point selection
+        if self.method == 'smart' and force_random == False:
+            nextTarget = self.SmartTargetSelect(coverage, brush, robot_pose, resolution, origin, nodes)
 
+            # Check if SmartTargetSelect found a target
+            if nextTarget is not None: 
+                # Check if the next target is the same as the previous
+                dist = dist = math.hypot(\
+                    nextTarget[0] - self.previous_target[0], \
+                    nextTarget[1] - self.previous_target[1])
+                if dist > 5:
+                    target = nextTarget
+                else:
+                    target = self.selectRandomTarget(ogm, coverage, brush, ogm_limits)
+            else:
+                # No target found. Choose a random
+                target = self.selectRandomTarget(ogm, coverage, brush, ogm_limits)
+
+
+        self.previous_target = target
         return target
+
+
+
+    def SmartTargetSelect(self, coverage, brush, robot_pose, resolution, origin, nodes):
+        tinit = time.time()
+
+        # Get the robot pose in pixels
+        [rx, ry] = [\
+            int(round(robot_pose['x_px'] - origin['x'] / resolution)),\
+            int(round(robot_pose['y_px'] - origin['y'] / resolution)) \
+                ]
+        
+        weights = []
+
+        for i, node in enumerate(nodes):
+            # Calculate the path
+            path = np.flip(self.path_planning.createPath([rx, ry], node, resolution), 0)
+
+            # Check if it found a path
+            if path.shape[0] > 2:
+                # Vectors of the path
+                vectors = path[1:, :] - path[:-1, :]
+
+                # Calculate paths weighted distance
+                vectorsMean = vectors.mean(axis=0)
+                vectorsVar = vectors.var(axis=0)
+                dists = np.sqrt(np.einsum('ij,ij->i', vectors, vectors))
+                weightCoeff = 1 / (1 - np.exp(-np.sum((vectors - vectorsMean)**2 / (2 * vectorsVar), axis=1)) + 1e-4)
+                weightDists = np.sum(weightCoeff + dists)
+
+                # Topological weight
+                weightTopo = brush[node[0], node[1]]
+
+                # Cosine of the angles
+                c = np.sum(vectors[1:, :] * vectors[:-1, :], axis=1) \
+                             / np.linalg.norm(vectors[1:, :], axis=1) \
+                             / np.linalg.norm(vectors[:-1, :], axis=1)
+
+                # Sum of all angles
+                weightTurn = np.sum(abs(np.arccos(np.clip(c, -1, 1))))
+
+                # Calculate the coverage weight 
+                pathIndex = np.rint(path).astype(int)
+                weightCove = 1 - np.sum(coverage[pathIndex[:, 0], pathIndex[:, 1]]) / (path.shape[0] * 100)
+
+                weights.append([i, weightDists, weightTopo, weightTurn, weightCove])
+            
+
+        if len(weights) > 0:
+            weight = np.array(weights)
+
+            # Normalize the weights
+            weight[:,1:] = 1 - ((weight[:,1:] - np.min(weight[:,1:], axis=0)) / \
+                        (np.max(weight[:,1:], axis=0) - np.min(weight[:,1:], axis=0)))
+
+            # Calculatete the final weights
+            finalWeights = 8 * weight[:, 2] + 4 * weight[:, 1] + 2 * weight[:, 4] + weight[:, 3]
+
+            # Find the best path
+            index = int(weight[max(xrange(len(finalWeights)), key=finalWeights.__getitem__)][0])
+
+            target = nodes[index]
+
+            Print.art_print("Smart target selection time: " + str(time.time() - tinit), \
+            Print.ORANGE)
+
+            return target
+        else:
+            Print.art_print("Smart target selection failed!!! Time: " + str(time.time() - tinit), \
+            Print.ORANGE)
+
+            return None
+        
+
+    ########################################################################
 
     def selectRandomTarget(self, ogm, coverage, brushogm, ogm_limits):
       # The next target in pixels
